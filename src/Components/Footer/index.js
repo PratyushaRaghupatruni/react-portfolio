6d50ec13c440c1be87cc01c777aec618f3be5746
pratyu <pratyusha.raghupatruni@gmail.com>
2020-12-10 01:31:14 -0800

    added copywright to the footer

--- src/Components/Footer/index.js
+++ src/Components/Footer/index.js
@@ -8,19 +8,26 @@ import './style.css';
 export default function Footer() {
     return (
         <>
-            <footer class='footer mt-auto py-3 fixed-bottom'>
-                <div class='container' >
+            <footer className='footer mt-auto py-3 fixed-bottom'>
+                <div className ='container mb-3' >
                    <a href='https://www.linkedin.com/in/pratyusha-raghupatruni-96a35832/' ><i class='fa fa-linkedin linkedln' 
-                      style={{ fontSize:'36px',color :'black',marginRight : '15px'}}></i></a>
+                      style={{ fontSize:'30px',color :'black',marginRight : '30px'}}></i></a>
                    <a href='https://github.com/PratyushaRaghupatruni'><i class='fa fa-github github'  
-                      style={{ fontSize:'36px',color :'black',marginRight : '15px'}}></i></a>
+                      style={{ fontSize:'30px',color :'black',marginRight : '30px'}}></i></a>
                    <a href='mailto:pratyusha.raghupatruni@gmail.com'><i class='fa fa-envelope mail'
-                       style={{fontSize:'36px',color:'black', marginRight : '15px',}}></i></a>
+                       style={{fontSize:'30px',color:'black', marginRight : '30px',}}></i></a>
                   <a href='https://docs.google.com/document/d/10B8XLsG32NMiA7ZvEqN4rH3jcebooHYqEGTxERoeVEU/edit '><i class="fa fa-file-text"
-                       style={{fontSize:'36px',color:'black'}}></i></a>
+                       style={{fontSize:'30px',color:'black'}}></i></a>
+                <br />
+                <p className="copyright mt-2" style={{ fontSize:'1.0em',color :'black',marginRight : '30px'}}>
+                  ©{" "}
+                   <a className="link-pratyusha text-center" href="https://www.linkedin.com/in/pratyusha-raghupatruni-96a35832/">
+                    Pratyusha Raghupatruni
+                   </a>{" "}
+                    2020
+                </p>
               </div>
-  </footer>
-             
+         </footer>     
         </>
     )
 }

6e28ba7f61cd7253efc0b7508e37e47b5f4e197e
pratyu <pratyusha.raghupatruni@gmail.com>
2021-01-06 00:03:17 -0800

    modified the routes of components

--- src/App.js
+++ src/App.js
@@ -4,29 +4,18 @@ import './App.css';
 // import Main from './Pages/Main/main';
 import Nav from './Components/Nav';
 import Footer from './Components/Footer'
-import ProjectList from './Pages/ProjectList/projectlist';
+import ProjectList from './Components/ProjectList';
 import About from './Components/About';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router }from 'react-router-dom';
 
 function App() {
   return (
 		<div className='background-img'>
-   <Router>
-				{/* Router */}
-			 <Nav />
-				<Switch>
-					{/* <Route exact path="/" exact component={Main} />
-					<Route path="/portfolio" exact component={ProjectList} />
-					<Route path="/about" exact component={About} /> */}
-					 <Route exact path={"/"}>
-              <About />
-            </Route>
-				 	  <Route exact path="/portfolio"><ProjectList /></Route>
-					  <Route>
-              <About />
-            </Route>
-				</Switch>
-       <Footer />
+        <Router>
+			     <Nav />
+			     <About />	
+			  	 <ProjectList />
+           <Footer />
 		</Router>
 		</div>
   );

9308bd845871a4b578941e9cfb07e4243ee8820a
pratyu <pratyusha.raghupatruni@gmail.com>
2021-01-09 20:23:48 -0800

    added new components in appjs file

--- src/App.js
+++ src/App.js
@@ -7,17 +7,23 @@ import Home from './Components/Home';
 import Footer from './Components/Footer';
 import ProjectList from './Components/ProjectList';
 import About from './Components/About';
+import TechStack from './Components/TechStack';
+import Adjuster from './Components/Adjuster';
 import { BrowserRouter as Router }from 'react-router-dom';
 
 function App() {
   return (
 		<div className='App'>
         <Router>
-			     <Navbar />
-                  <Home />
-			      <About />	
-			  	  <ProjectList />
-                  <Footer />
+			          
+        <Navbar />
+        <Home />
+        <About/>
+		<TechStack />
+		<Adjuster />
+        <ProjectList/>
+        <Footer />
+    
 		</Router>
 		</div>
   );

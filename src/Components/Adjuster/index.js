ea769d82793c8cd85cbfc964d85f5bc96e1d4d12
pratyu <pratyusha.raghupatruni@gmail.com>
2021-01-09 20:31:11 -0800

    added for background adjuster

---
+++ src/Components/Adjuster/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../../assets/css/style.css';
+
+export default function Adjuster() {
+    return (
+        <div className ="color-adjuster">
+            <br></br>
+             <br></br>
+            <br></br>
+        </div>
+    )
+}

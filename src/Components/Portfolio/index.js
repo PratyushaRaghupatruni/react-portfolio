4f0552e5f89db31f762d7fe722d11e46662136fc
pratyu <pratyusha.raghupatruni@gmail.com>
2021-02-03 16:43:43 -0800

    changed the design of the icons in portfolio

--- src/Components/Portfolio/index.js
+++ src/Components/Portfolio/index.js
@@ -21,26 +21,18 @@ export default function Portfolio(props) {
                 </Card.Title>
                 <Card.Text style={{fontSize:'1.2em', textAlign: 'left',color: '#444444',padding: '15px'}}>{props.project.description}</Card.Text>
                
-                <div className='float-left'>
-                  <button className='btn-grad ' style={{ minWidth: '60px',margin: '0px 0px 10px 20px',}}>Details</button>
-                </div> 
-                 
-                 <div className="float-right " style={{ margin: '8px 0px 20px 20px',}}>
                      <a  style={{ minWidth: '60px',margin: '0px 0px 20px 20px',}}
                       href={props.project.appUrl}
                       target="_blank"
-                      rel="noopener noreferrer"
-                     >
-                       <i className ='fas fa-globe globeicon ' style={{fontSize:'30px'}}></i>
+                      rel="noopener noreferrer">
+                       <i className ='fas fa-globe icon-card ' style={{fontSize:'30px'}}></i>
                     </a>
-                  <a   style={{ minWidth: '60px',margin: '0px  0px 20px 20px',}}
+                    <a  style={{ minWidth: '60px',margin: '0px  0px 20px 20px',}}
                     href={'https://github.com/' + props.project.id}
                     target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    <i className ='fa fa-github githubicon ' style={{fontSize:'36px'}}></i>
+                    rel="noopener noreferrer">
+                    <i className ='fa fa-github icon-card ' style={{fontSize:'36px'}}></i>
                   </a>
-                  </div>
               </Card.Body>
             </Card>
           </Col>
